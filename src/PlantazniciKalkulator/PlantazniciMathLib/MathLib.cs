2a470c1c3d82ef55b89594e2c75097c6cc8cbdde
TheRandomYellowSlime <116088741+TheRandomYellowSlime@users.noreply.github.com>
2023-04-14 19:09:36 +0200

    Dokumentace: MathLib
    Přidání dokumentace matematické knihovny

--- src/PlantazniciKalkulator/PlantazniciMathLib/MathLib.cs
+++ src/PlantazniciKalkulator/PlantazniciMathLib/MathLib.cs
@@ -1,12 +1,26 @@
 ﻿/*
+ * Projekt: Plantážníci - Kalkulačka
+ *
+ * Soubor:  MathLib.cs
+ * 
+ * Tým:     Plantážníci
+ *
  * Autoři:  Marek Kunz (xkunzm02) - vedouci
  *          Jan Křivák (xkriva36)
  *          Marian Pražák (xpraza11)
  *          Kryštof Pleva (xpleva09)
  * 
- * Tým:     Plantážníci
+ * Popis:   Třída pro knihovnu matematických operací
  * 
- * */
+ */
+/**
+ * @file MathLib.cs
+ * 
+ * @brief Knihovna matematických operací
+ * 
+ * @author Marek Kunz, Jan Křivák, Marian Pražák, Kryštof Pleva
+ *
+ */
 
 using System;
 using System.Collections.Generic;
@@ -16,23 +30,51 @@ using System.Threading.Tasks;
 
 namespace Plantaznici.Kalkulacka.MathLib
 {
+    /// <summary>
+    /// Třída pro knihovnu matematických operací
+    /// </summary>
     public class MatematickaKnihovna
     {
+        /// <summary>
+        /// Funkce pro sčítání dvou čísel
+        /// </summary>
+        /// <param name="scitanecA"> První sčítané číslo </param>
+        /// <param name="scitanecB"> Druhé sčítané číslo </param>
+        /// <returns> Vrací součet vstupních hodnot</returns>
         public double Scitani(double scitanecA, double scitanecB)
         {
             return scitanecA + scitanecB;
-        }
+        }   // Koniec funkce Scitani
 
+        /// <summary>
+        /// Funkce pro odčítání dvou čísel
+        /// </summary>
+        /// <param name="mensenec"> Základ, od kterého odčítáme </param>
+        /// <param name="mensitel"> Hodnota, kterou odčítáme od základu </param>
+        /// <returns> Vrací rozdíl vstupních hodnot </returns>
         public double Odcitani(double mensenec, double mensitel)
         {
             return mensenec - mensitel;
-        }
+        }   // Koniec funkce Odcitani
 
+        /// <summary>
+        /// Funkce pro násobení dvou čísel
+        /// </summary>
+        /// <param name="cinitelA"> První násobené číslo </param>
+        /// <param name="cinitelB"> Druhé násobené číslo </param>
+        /// <returns> Vrací součin vstupních hodnot </returns>
         public double Nasobeni(double cinitelA, double cinitelB)
         {
             return cinitelA * cinitelB;
-        }
+        }   // Koniec funkce Nasobeni
 
+        /// <summary>
+        /// Funkce pro dělení dvou čísel
+        /// </summary>
+        /// <exception cref="DivideByZeroException"> Vyvolá výjimku, pokud je dělitel nulou </exception>
+        /// <param name="delenec"> Základ, od který dělíme </param>
+        /// <param name="delitel"> Hodnota, kterou dělíme základ </param>
+        /// <returns> Vrací podíl vstupních hodnot </returns>
         public double Deleni(double delenec, double delitel)
         {
             if (delitel == 0)
@@ -43,8 +85,23 @@ namespace Plantaznici.Kalkulacka.MathLib
             {
                 return delenec / delitel;
             }
-        }
+        }   // Koniec funkce Deleni
 
+        /// <summary>
+        /// Funkce pro výpočet faktoriálu čísla
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         <list type="bullet">
+        ///            <item>
+        ///                <term> Výchozí hodnota návratové hodnoty je 1  </term>
+        ///            </item>
+        ///         </list>
+        ///     </para>
+        /// </remarks>
+        /// <exception cref="ArgumentException"> Vyvolá výjimku, pokud je vstupní hodnota záporná nebo desetinná </exception>
+        /// <param name="n"> Číslo, pro které hledáme faktoriál </param>
+        /// <returns> Vrací hodnotu faktoriálu ze vstupní hodnoty </returns>
         public double Faktorial(double n)
         {
             if (n >= 0)
@@ -67,8 +124,36 @@ namespace Plantaznici.Kalkulacka.MathLib
             {
                 throw new ArgumentException("Faktorial zaporneho cisla neni definovan");
             }
-        }
+        }   // Koniec funkce Faktorial
 
+        /// <summary>
+        /// Funkce pro výpočet mocniny čísla
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         <list type="bullet">
+        ///            <item>
+        ///                <term> Výchozí hodnota návratové hodnoty je 1  </term>
+        ///            </item>
+        ///         </list>
+        ///     </para>
+        ///     <para>
+        ///         Využité externí funkce:
+        ///     </para>
+        ///     <para>
+        ///         <list type="table">
+        ///             <item>
+        ///                 <term> Zaokrouhleni </term>
+        ///                 <description> Využita pro zaokrouhlení exponentu na celé číslo </description>
+        ///             </item>
+        ///         </list>
+        ///     </para>
+        /// </remarks>
+        /// <exception cref="ArgumentException"> Vyvolá výjimku, pokud je základ i exponent nulou </exception>
+        /// <exception cref="ArgumentException"> Vyvolá výjimku, pokud je základ nulou a exponent je záporný </exception>
+        /// <param name="zaklad"> Základ, který umocňujeme </param>
+        /// <param name="exponent"> Exponent, kterým umocňujeme základ</param>
+        /// <returns> Vrací hodnotu po umocnění základu exponentem </returns>
         public double Umocneni(double zaklad, double exponent)
         {
             exponent = Zaokrouhleni(exponent);
@@ -112,8 +197,38 @@ namespace Plantaznici.Kalkulacka.MathLib
             }
 
             return vysledek;
-        }
+        }   // Konec funkce Umocneni
 
+        /// <summary>
+        /// Funkce pro výpočet odmocniny čísla
+        /// </summary>
+        /// <remarks>
+        ///     <para>
+        ///         Využité externí funkce:
+        ///     </para>
+        ///     <para>
+        ///         <list type="table">
+        ///             <item>
+        ///                 <term> Zaokrouhleni </term>
+        ///                 <description> Využita pro zaokrouhlení exponentu na celé číslo </description>
+        ///             </item>
+        ///             <item>
+        ///                 <term> AbsHodnota </term>
+        ///                 <description> Využita při kontrole odchilky při výpočtu odmocniny </description>
+        ///             </item>
+        ///             <item>
+        ///                 <term> Umocneni </term>
+        ///                 <description> Využita při výpočtu odmocniny </description>
+        ///             </item>
+        ///         </list>
+        ///     </para>
+        /// </remarks>
+        /// <exception cref="ArgumentException"> Vyvolá výjimku, pokud je exponent nulový </exception>
+        /// <exception cref="ArgumentException"> Vyvolá výjimku, pokud je základ nulový a exponent je záporný </exception>
+        /// <exception cref="ArgumentException"> Vyvolá výjimku, pokud je základ záporný a exponent je sudý </exception>
+        /// <param name="zaklad"> Základ, který odmocňujeme </param>
+        /// <param name="exponent"> Exponent, kterým odmocňujeme základ </param>
+        /// <returns> Vrací hodnotu po odmocnění základu exponentem </returns>
         public double Odmocneni(double zaklad, double exponent)
         {
             exponent = Zaokrouhleni(exponent);
@@ -157,8 +272,13 @@ namespace Plantaznici.Kalkulacka.MathLib
                 x = ((exponent - 1.0) * x + zaklad / Umocneni(x, exponent - 1)) / exponent;
             } while (AbsHodnota(x - xPredchozi) > odchylka);
             return x;
-        }
+        }   // Konec funkce Odmocneni
 
+        /// <summary>
+        /// Funkce pro výpočet absolutní hodnoty čísla
+        /// </summary>
+        /// <param name="n"> Číslo, pro které hledáme absolutní hodnotu </param>
+        /// <returns> Vrací hodnotu absolutní hodnoty ze vstupní hodnoty </returns>
         public double AbsHodnota(double n)
         {
             if (n < 0)
@@ -169,8 +289,13 @@ namespace Plantaznici.Kalkulacka.MathLib
             {
                 return n;
             }
-        }
+        }   // Konec funkce AbsHodnota
 
+        /// <summary>
+        /// Funkce pro zaokrouhlení čísla
+        /// </summary>
+        /// <param name="n"> Číslo, které chceme zaokrouhlit </param>
+        /// <returns> Vrací zaokrouhlenou hodnotu vstupní hodnoty </returns>
         public double Zaokrouhleni(double n)
         {
             double x = (int)n;
@@ -199,6 +324,6 @@ namespace Plantaznici.Kalkulacka.MathLib
                     return x;
                 }
             }
-        }
+        }   // Konec funkce Zaokrouhleni
     }
 }
